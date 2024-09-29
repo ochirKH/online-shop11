8314d6468d0473935edde95d923102f555556629
ochirKH <ochir03.1998@gmail.com>
2024-09-29 23:20:42 +0800

    new index

---
+++ src/public/index.php
@@ -0,0 +1,6 @@
+<?php
+//$pdo = new PDO("pgsql:host=postgres; port=5432; dbname=name", "user", "pwd");
+//$stmt = $pdo->prepare("SELECT * FROM users");
+//$result = $stmt->fetchAll();
+//print_r($result);
+echo "hello world!";
